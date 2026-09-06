4f925c6802facbc6544d367a59969a8150e0a116
zahib <muhammadzahib99@gmail.com>
2026-09-06 19:37:25 +0500

    make app respoinsive in landscape mode and ui improvements

--- app/src/main/java/com/example/marginal/presentation/auth/LoginScreen.kt
+++ app/src/main/java/com/example/marginal/presentation/auth/LoginScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -48,6 +49,7 @@ fun LoginScreen(
         modifier = Modifier
             .fillMaxSize()
             .background(Paper)
+            .statusBarsPadding()
             .verticalScroll(rememberScrollState())
             .padding(horizontal = 26.dp),
         verticalArrangement = Arrangement.Center,
